da0a1d9537f3cdc1597bf36568a1d1caa5b61438
yasebo2-crypto <yasebo2@gmail.com>
2026-07-01 17:38:48 +0300

    Add files via upload

---
+++ _next/static/5thJMwEfL-M3dWK3PWIrE/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fcheckout","\u002F[locale]\u002Fcontacto","\u002F[locale]\u002Fproduct\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
